d4dee45d112e4a0358e0904e20ae410e14b88531
Andrew S <andrews54757@gmail.com>
2018-03-18 13:05:29 -0400

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,10 @@
+/*
+Copyright @c Andrews54757 (andrews54757@gmail.com)
 
+This script is educational but is protected so people dont sell it/misuse it.
+
+Please do not ruin the game
+
+Thank you!
+
+*/
